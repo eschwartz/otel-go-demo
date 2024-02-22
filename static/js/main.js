039824a8a551f7061ab17be4879a36463d3461c3
Edan Schwartz <edanschwartz@gmail.com>
2024-02-22 11:56:40 -0600

    minor tracing attr changes

--- static/js/main.js
+++ static/js/main.js
@@ -14,7 +14,6 @@ const withActiveSpan = (name, fn) => {
             span.setStatus({ code: SpanStatusCode.ERROR, message: err.message });
             span.setAttributes({
                 "app.error": err.message,
-                "app.error.stack": err.stack,
             })
             throw err
         }
@@ -55,32 +54,24 @@ const request = async (url, opts = {}) => {
 
 
 document.getElementById("search").onsubmit = async (evt) => {
-    evt.preventDefault()
-    withActiveSpan("submit fetch", async span => {
+    evt. preventDefault()
+
+    withActiveSpan("form submit", async span => {
         const limit = document.getElementById("limit").value
-        const term = document.getElementById("term").value
+        const q = document.getElementById("term").value
 
-        span.setAttributes({
-            "app.fetchTrigger": "search",
-            "app.itemsSearch.limit": limit,
-            "app.itemsSearch.term": term,
-        })
+        let items = await fetchItems(q, limit)
 
-        const res = await request(`/api/items?limit=${limit}&q=${term}`)
-        const items = await res.json()
-
-        span.setAttributes({
-            'app.resultCount': items.length,
-            'app.resultJson': JSON.stringify(items)
-        })
-
-        renderItems(items)
+        // Render the items
+        document.getElementById("items")
+            .innerHTML = items.map(item => `<li>${item.value}</li>`).join('')
     })
-
-    return false;
 }
 
 document.getElementById("lucky").onclick = async () => {
+    // fetchItems('bicycle', 1);
+    // return;
+
     withActiveSpan("submit fetch", async span => {
         // Pick a random limit between 0 and 3
         const randomLimit = Math.round(Math.random() * 3)
@@ -109,4 +100,40 @@ document.getElementById("lucky").onclick = async () => {
 function renderItems(items) {
     document.getElementById("items")
         .innerHTML = items.map(item => `<li>${item.value}</li>`).join('')
+}
+
+async function fetchItems(q, limit) {
+    // Create a new trace span
+    return withActiveSpan('fetch /api/items', async span => {
+        // Add attributes to the span
+        span.setAttributes({
+            'app.request.q': q,
+            'app.request.limit': limit,
+        })
+
+        // Fetch the items
+        const res = await fetch(`/api/items?q=${q}&limit=${limit}`, {
+            headers: {
+                // Propagate our trace via an HTTP header
+                traceparent: `00-${span.spanContext().traceId}-${span.spanContext().spanId}-01`,
+            }
+        })
+
+        // Handle HTTP error codes
+        if (res.status >=400) {
+            span.setAttribute(`app.response.body`, await res.text())
+            throw new Error(`Unexpected HTTP ${res.status} response`)
+        }
+
+        // Deserialize JSON response
+        const items = await res.json()
+
+        // Update the span with more attributes from the response
+        span.setAttributes({
+            'app.response.count': items.length,
+            'app.response.status': res.status,
+        })
+
+        return items
+    })
 }
